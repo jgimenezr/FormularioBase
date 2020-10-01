01620105881dd129f1ad28b49ac873c5ec6dc667
jgimenezr <63068304+jgimenezr@users.noreply.github.com>
2020-10-01 12:56:13 -0400

    actualizaciones
    actualizaciones

--- send.php
+++ send.php
@@ -1,65 +1,66 @@
 <?php
 
-//librerias
-  require 'PHPMailer/PHPMailerAutoload.php';
+	//Créditos de uso de PHPMailer - extraido del github de Victor García https://github.com/victorgarciasisi/enviarEmailPHP
+	//librerias
+	require 'PHPMailer/PHPMailerAutoload.php';
 
-//Creación de una nueva instancia de PHPMailer
-¿
-$mail = new PHPMailer();
-$mail->IsSMTP();
+	//Creación de una nueva instancia de PHPMailer	
 
-//Configuracion servidor mail
-$mail->From = "jjgimenezprueba@gmail.com"; //remitente
-$mail->SMTPAuth = true;
-$mail->SMTPSecure = 'tls'; //seguridad
-$mail->Host = "smtp.gmail.com"; // servidor smtp
-$mail->Port = 587; //puerto
-$mail->Username ='jjgimenezprueba@gmail.com'; //nombre usuario
-$mail->Password = 'Hola.321'; //contraseña
-$mail->FromName = "Juan José Gimenez";
+	$mail = new PHPMailer();
+	$mail->IsSMTP();
 
-$comentarios = "Nombres: ".$_POST['nombre']."\n";
-$comentarios .= "Apellidos: ".$_POST['apellido']."\n";
-$comentarios .= "Correo: ".$_POST['email']."\n";
-$comentarios .= "Comentario: ".$_POST['comentario'];
+	//Configuracion servidor mail
+	$mail->From = "jjgimenezprueba@gmail.com"; //remitente
+	$mail->SMTPAuth = true;
+	$mail->SMTPSecure = 'tls'; //seguridad
+	$mail->Host = "smtp.gmail.com"; // servidor smtp
+	$mail->Port = 587; //puerto
+	$mail->Username ='jjgimenezprueba@gmail.com'; //nombre usuario
+	$mail->Password = 'Hola.321'; //contraseña
+	$mail->FromName = utf8_decode("Juan José Giménez");
 
-//Agregar destinatario
-$mail->AddAddress("xymenez92@gmail.com");
-$mail->Subject = "Datos Formulario Web Juan Jose Gimenez";
-$mail->Body = $comentarios;
+	$comentarios = "Nombres: ".$_POST['nombre']."\n";
+	$comentarios .= "Apellidos: ".$_POST['apellido']."\n";
+	$comentarios .= "Correo: ".$_POST['email']."\n";
+	$comentarios .= "Comentario: ".$_POST['comentario'];
 
-//Avisar si fue enviado o no y dirigir al index
-if ($mail->Send()) {
+	//Agregar destinatario
+	$mail->AddAddress("ivangarcete@gmail.com");
+	$mail->Subject = utf8_decode("Datos Formulario Web Juan Jose Giménez");
+	$mail->Body = $comentarios;
 
-	echo '<script type="text/javascript">
+	//Avisar si fue enviado o no y dirigir al index
+	if ($mail->Send()) {
 
-	alert("Enviado Correctamente");
+		echo '<script type="text/javascript">
 
-    if ( window.history.replaceState){
+		alert("Enviado Correctamente");
+    
+		if (window.history.replaceState){
 
-    	window.history.replaceState( null, null, null, null, window.location.href );
+			window.history.replaceState( null, null, null, null, window.location.href );
 
-    }
+		}
     		
-    window.location = "index.php";
+		window.location = "index.html";
 
-    </script>';
+		</script>';
 
-} else {
+	}else{
 
-	echo '<script type="text/javascript">
+		echo '<script type="text/javascript">
 
-	alert("No se enviaron los datos");
+		alert("No se enviaron los datos");
 
-    if ( window.history.replaceState ){
+		if (window.history.replaceState ){
 
-    	window.history.replaceState( null, null, null, null, window.location.href );
+			window.history.replaceState( null, null, null, null, window.location.href );
 
-    }
+		}
     		
-    window.location = "index.php";
+		window.location = "index.html";
 
-    </script>';
+		</script>';
 
 
 
